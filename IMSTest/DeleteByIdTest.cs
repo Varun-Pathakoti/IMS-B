70cc5d5fed01d179247761d44bb6c9184217ec09
Varun-Pathakoti <varun.kumar.vk997@gmail.com>
2024-07-18 17:35:33 +0530

    test method

---
+++ IMSTest/DeleteByIdTest.cs
@@ -0,0 +1,37 @@
+﻿using IMSDataAccess;
+using IMSDomain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMSTest
+{
+    [TestClass]
+    public class DeleteByIdTest
+    {
+        private DbContextOptions<ProductDbContext> GetInMemoryDbContextOptions()
+        {
+            return new DbContextOptionsBuilder<ProductDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+        }
+        [TestMethod]
+        public async Task DeletesProduct()
+        {
+            var options = GetInMemoryDbContextOptions();
+            using var context = new ProductDbContext(options);
+            var inventory = new InventoryRepository(context);
+            context.Products.Add(new Product { ProductID = 1, ProductName = "TestProduct", Description = "TestDescription", StockLevel = 10, Price = 100, Threshold = 5 });
+            context.SaveChanges();
+            await inventory.DeleteById(1);
+
+            Assert.AreEqual(0, context.Products.Count());
+        }
+
+
+    }
+}
+
